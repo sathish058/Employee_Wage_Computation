c20ff616f45a1ce880ed7d3d478bc62e78371732
Sathish Saravanan <sathishsaravanan@Sathishs-MacBook-Air.local>
2022-04-22 23:40:15 +0530

    Added UC_02

---
+++ EmpDailyWage.java
@@ -0,0 +1,37 @@
+package com.bl.employeewage;
+
+/**
+ * @author Sathish
+ *
+ */
+	public class EmpDailyWage {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Employee wage computation program..!");
+ 
+		int isPresent = 1;
+		int EmpRatePerHour = 20;
+		int empHrs = 0;
+		int empWage = 0;
+
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+
+		if (empCheck == isPresent)
+			System.out.println("\nEmployee is present !");
+		else
+			System.out.println("\nEmployee is absent..");
+
+		if (empCheck == isPresent)
+			empHrs = 8;
+		else
+			empHrs = 0;
+		empWage = empHrs * EmpRatePerHour;
+		System.out.println("Employee wages will be : " + empWage + " INR");
+	}
+}
+
+
+	
+
+
